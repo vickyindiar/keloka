23809d5780efa50c74bd5802340cb32f5958d721
vickyindiar <vickynewonline@gmail.com>
2019-08-12 22:26:48 +0700

    update

--- src/services/actions/dataAction.js
+++ src/services/actions/dataAction.js
@@ -16,11 +16,11 @@ export const changeTabIndex = tab => dispatch => {
     if(tab === 0) { 
       url = 'http://127.0.0.1:8000/api/product'; 
       columns = [
-        { id: "name", field: "name", caption: "Dessert (100g serving)", align: "left", disablePadding: true },
-        { id: "brand", field: "brand", caption: "Brand", align: "left", disablePadding: false },
-        { id: "price", field: "price", caption: "Price", align: "right", disablePadding: false },
-        { id: "stock", field: "stock", caption: "Stock", align: "right", disablePadding: false },
-        { id: "supplier", field: "supplier", caption: "Supplier", align: "left", disablePadding: false }
+        { id: "name", field: "name", caption: "Name", align: "center", disablePadding: true },
+        { id: "brand", field: "brand", caption: "Brand", align: "center", disablePadding: false },
+        { id: "price", field: "price", caption: "Price", align: "center", disablePadding: false },
+        { id: "stock", field: "stock", caption: "Stock", align: "center", disablePadding: false },
+        { id: "supplier", field: "supplier", caption: "Supplier", align: "center", disablePadding: false }
       ];
     } 
     axios.get(url, config).then(res =>{
@@ -29,11 +29,6 @@ export const changeTabIndex = tab => dispatch => {
             type: SET_TAB,
             payload: {  tabActive: tab, columns:columns, dataSource: res.data.data  }
           });
-
-          // dispatch({
-          //   type: SET_TAB,
-          //   payload: {  tabActive: tab, columns: res.data.columns, dataSource: res.data.data  }
-          // });
         }else{
             dispatch({ type: SET_TAB, payload: {  tabActive: tab, columns: {}, dataSource: {} }});
         }
